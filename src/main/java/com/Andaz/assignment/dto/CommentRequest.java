ba1be5eea3a0c38508f0489818ea6e465217d5af
Shivam <shivamfeb77@gmail.com>
2026-04-22 10:58:15 +0530

    added dto for comments

---
+++ src/main/java/com/Andaz/assignment/dto/CommentRequest.java
@@ -0,0 +1,30 @@
+package com.Andaz.assignment.dto;
+
+import com.Andaz.assignment.entity.AuthorType;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class CommentRequest {
+
+    @NotNull(message = "id is requirred")
+    private Long id;
+
+    @NotNull(message = "authorType is required(user or bot)")
+    private AuthorType authorType;
+
+
+    @NotBlank(message = "content must not be blank")
+    private String content;
+
+    @Min(value = 1, message = "depthLevel must be at least 1")
+    private Integer depthLevel = 1;
+
+
+    /* id can be null for top level comments */
+    private Long parentCommentId;
+    
+}
