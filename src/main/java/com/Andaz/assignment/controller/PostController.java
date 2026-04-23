56cc3cce32e9d1cded9763f424fa88f1a19c00b9
Shivam <shivamfeb77@gmail.com>
2026-04-23 22:44:20 +0530

    added controller for post

--- src/main/java/com/Andaz/assignment/controller/PostController.java
+++ src/main/java/com/Andaz/assignment/controller/PostController.java
@@ -2,13 +2,17 @@ package com.Andaz.assignment.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Andaz.assignment.dto.CommentRequest;
+import com.Andaz.assignment.dto.CommentResponse;
 import com.Andaz.assignment.dto.CreatePostRequest;
 import com.Andaz.assignment.dto.PostResponse;
+import com.Andaz.assignment.service.CommentService;
 import com.Andaz.assignment.service.PostService;
 
 import jakarta.validation.Valid;
@@ -20,6 +24,7 @@ import lombok.AllArgsConstructor;
 public class PostController {
 
     private final PostService postService;
+    private final CommentService commentService;
 
 
 
@@ -29,6 +34,15 @@ public class PostController {
        
         PostResponse response =  postService.createPost(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+    /* PostMapping to add the comment to the post */
+    @PostMapping("/{postid}/comments")
+    public ResponseEntity<CommentResponse> addComment(@PathVariable Long postid, @Valid @RequestBody CommentRequest request){
+       CommentResponse response =  commentService.addComment(postid, request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        
     }
     
 }
