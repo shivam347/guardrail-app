846b1ebed4b59c07a8896ccd90a043fb4d149440
Shivam <shivamfeb77@gmail.com>
2026-04-24 11:08:03 +0530

    update bot repository

--- src/main/java/com/Andaz/assignment/repository/BotRepository.java
+++ src/main/java/com/Andaz/assignment/repository/BotRepository.java
@@ -1,9 +1,11 @@
 package com.Andaz.assignment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Andaz.assignment.entity.Bot;
 
+@Repository
 public interface BotRepository extends JpaRepository<Bot, Long> {
 
     boolean existsByName(String name);
