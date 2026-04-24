4811f4549ebeb234f6d2615515b4050aadaa27c4
Shivam <shivamfeb77@gmail.com>
2026-04-24 11:08:48 +0530

    update comment repo

--- src/main/java/com/Andaz/assignment/repository/CommentRepository.java
+++ src/main/java/com/Andaz/assignment/repository/CommentRepository.java
@@ -1,9 +1,11 @@
 package com.Andaz.assignment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.Andaz.assignment.entity.Comment;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
 
